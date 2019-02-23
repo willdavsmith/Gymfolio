9240b83be6982e6188571c8b139ad8d5375268ad
smithwid <smithwid@iu.edu>
2019-02-23 12:12:11 -0500

    initial commit

---
+++ app/src/main/java/edu/indiana/gymfolio/GymfolioCustomizeActivity.java
@@ -0,0 +1,13 @@
+package edu.indiana.gymfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GymfolioCustomizeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gymfolio_customize);
+    }
+}
